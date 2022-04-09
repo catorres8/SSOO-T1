cdc9cb1cfa508f0097e75610443b9b2029a8906a
catorres8 <catorres8@uc.cl>
2022-04-09 11:56:28 -0400

    Queue Definition

--- T2/src/queue/queue.h
+++ T2/src/queue/queue.h
@@ -1,5 +1,8 @@
 // Tells the compiler to compile this file once
 #pragma once
+#include <stdlib.h>
+#include <stdio.h>
+#include "../process/process.h"
 
 // Define compile-time constants
 
@@ -7,34 +10,89 @@
 typedef struct queue
 {
     int queue_type; // 0 = FIFO; 1 = SJF
-    int quantum;
-    struct queue* next;
+
+    Process proceso; // cada elemento de la cola tiene un proceso
+    struct queue* next; // cada proceso tiene un siguiente proceso
     
-} Queue;
+} Queue; 
+
 
 // Declare functions
-Queue* init(int tipo){
-
+Queue* init(Process* proceso, int tipo)
+{
     Queue* cola = malloc(sizeof(Queue));
-
     *cola = (Queue) {
         .queue_type = tipo,
-        .processes = malloc(),
+        .proceso = proceso,
         .next = NULL,
     }
-    
     return cola
 }
 
-void append(Queue* cola, process* proceso) {
-    if (cola->queue_type == 0)
+
+void append(Queue* cola, Process* proceso) {
+    if (cola->queue_type == 0) // Aqui hacemos mecanica FIFO
     {
-        /* code */
+        // vamos al final de la cola
+        Queue *ultimo = cola;
+        while (ultimo->next) {
+            ultimo = ultimo->next;
+        }
+
+        // creamos el nuevo nodo
+        Queue *new_list = init(proceso, cola->queue_type);
+
+        // Lo agregamos como el nodo siguiente al ultimo nodo de la lista
+        ultimo->next = new_list;
     }
-    else
+    else                       // Aqui hacemos mecanica SJF
     {
-        /* code */
+        /* code 
+        - recorro cola, 
+        - y comparo mi proceso.quantum, con cada proceso,quantum de la cola 
+            - si mi proceso.quantum < a cada proceso.quantum de la cola:
+                - 
+        - luego la recorro, la menor, y agrego a otra lista ligada (en donde estaran ordenado por SFJ)
+        mmmm ta raro
+        */
     }
     
 }
 
+
+// CODIGO ANTERIOR
+
+// typedef struct queue
+// {
+//     int queue_type; // 0 = FIFO; 1 = SJF
+//     int priority;
+//     int quantum;
+//     struct queue* previous; // Rerferencia al elemento anterior
+//     struct queue* next;    // Referencia al elemento siguiente
+// } Queue;
+
+
+// Queue* init(int tipo, int prioridad, int input_q){
+//     Queue* cola = malloc(sizeof(Queue));
+//     *cola = (Queue) {
+//         .queue_type = tipo,
+//         .priority = prioridad,
+//         .quantum = prioridad * input_q,
+//         .processes = malloc(Process),
+//         .previous = NULL,
+//         .next = NULL,
+//     }
+//     return cola
+// }
+
+
+// void append(Queue* cola, process* proceso) {
+//     if (cola->queue_type == 0)
+//     {
+//         /* code */
+//     }
+//     else
+//     {
+//         /* code */
+//     }
+// }
